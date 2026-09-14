38cc7b8421c2532a2988126f173adb21bfb7169a
Gio-Arch <gio610gonzales@gmail.com>
2026-09-14 22:57:18 +0800

    WIP for this plugin from tej

--- lua/present.lua
+++ lua/present.lua
@@ -3,8 +3,8 @@ local function create_float(width, height)
 	local editor_height = vim.o.lines
 
 	-- Default to 80% of the current screen
-	width = width or math.floor(editor_width * 0.8)
-	height = height or math.floor(editor_height * 0.8)
+	width = width or editor_width
+	height = height or editor_height
 
 	-- Center the window
 	local row = math.floor((editor_height - height) / 2)
@@ -20,59 +20,66 @@ local function create_float(width, height)
 		height = height,
 		row = row,
 		col = col,
+		style = "minimal",
 		border = "rounded",
 	})
-
 	return buf, win
 end
-
-local _ = create_float()
 
 local M = {}
 
 M.setup = function() end
 
 --- @class present.Slides
---- @fields slides string[]
+--- @field slides present.Slide[]
+
+--- @class present.Slide
+--- @field title string
+--- @field body string[]
 
 ---@param lines string[]
 ---@return present.Slides
 local parse_slides = function(lines)
 	local slides = { slides = {} }
 	local separator = "^#"
-	local current_slides = {}
+	local current_bodies = {}
+	local title = ""
 
 	for _, line in ipairs(lines) do
-		print(line, "find:", line:find(separator), "|")
 		if line:find(separator) then
-			if #current_slides > 0 then
-				table.insert(slides.slides, current_slides)
+			if #current_bodies > 0 then
+				table.insert(slides.slides, { title = title, body = current_bodies })
 			end
-			current_slides = {}
+			title = line
+			current_bodies = {}
+		else
+			table.insert(current_bodies, line)
 		end
-		table.insert(current_slides, line)
 	end
 
-	table.insert(slides.slides, current_slides)
+	table.insert(slides.slides, { title = title, body = current_bodies })
 	return slides
 end
 
 M.start_presentation = function(opts)
 	opts = opts or {}
 
-	local current_slide = 0
+	local current_slide = 1
 	opts.bufnr = opts.bufnr or 0
 
 	local lines = vim.api.nvim_buf_get_lines(opts.bufnr, 0, -1, false)
 
 	local parsed = parse_slides(lines)
+	vim.print(parsed)
 
 	local float, win = create_float()
 
-	vim.api.nvim_buf_set_lines(float, 0, -1, false, parsed.slides[1])
+	vim.api.nvim_buf_set_lines(float, 0, -1, false, parsed.slides[current_slide])
+
+	vim.print("parsed slides length", #parsed.slides)
 
 	vim.keymap.set("n", "p", function()
-		current_slide = math.max(current_slide - 1, 0)
+		current_slide = math.max(current_slide - 1, 1)
 		vim.api.nvim_buf_set_lines(float, 0, -1, false, parsed.slides[current_slide])
 	end, { buffer = float })
 
@@ -82,10 +89,48 @@ M.start_presentation = function(opts)
 	end, { buffer = float })
 
 	vim.keymap.set("n", "q", function()
-		vim.nvim_win_close(win, true)
+		vim.api.nvim_win_close(win, true)
 	end, { buffer = float })
+
+	local restore = {
+		cmdheight = {
+			original = vim.o.cmdheight,
+			present = 0,
+		},
+	}
+	for option, config in pairs(restore) do
+		vim.opt[option] = config.present
+	end
+
+	vim.api.nvim_create_autocmd("BufLeave", {
+		buffer = float,
+		callback = function()
+			for option, config in pairs(restore) do
+				vim.opt[option] = config.original
+			end
+		end,
+	})
 end
 
-M.start_presentation({ bufnr = 137 })
+local scratch_buffer = vim.api.nvim_create_buf(true, true)
+local test_md = [[
+#Hello
+
+This is the first line
+
+#World
+
+This is the second line
+#Test
+
+This is the third line
+
+]]
+local lines = vim.split(test_md, "\n", { plain = true })
+
+vim.api.nvim_buf_set_lines(scratch_buffer, 0, -1, false, lines)
+vim.keymap.set("n", "<leader>x", function()
+	M.start_presentation({ bufnr = scratch_buffer })
+end)
 
 return M
